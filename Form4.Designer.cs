b328d9f84d2f3b86b6233135ceaf1795bea60262
rRaider002 <Julian.Hillebrecht1012@gmail.com>
2026-01-23 11:25:36 +0100

    Add files via upload

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -35,6 +35,11 @@ namespace Projekt
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tBNachname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTelefonnummer = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tbPresonen
@@ -46,7 +51,7 @@ namespace Projekt
             // 
             // tbName
             // 
-            this.tbName.Location = new System.Drawing.Point(355, 112);
+            this.tbName.Location = new System.Drawing.Point(355, 56);
             this.tbName.Name = "tbName";
             this.tbName.Size = new System.Drawing.Size(100, 20);
             this.tbName.TabIndex = 12;
@@ -63,11 +68,11 @@ namespace Projekt
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(292, 114);
+            this.label1.Location = new System.Drawing.Point(292, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(49, 13);
             this.label1.TabIndex = 10;
-            this.label1.Text = "Name";
+            this.label1.Text = "Vorname";
             // 
             // button2
             // 
@@ -88,11 +93,58 @@ namespace Projekt
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(382, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // tBNachname
+            // 
+            this.tBNachname.Location = new System.Drawing.Point(355, 95);
+            this.tBNachname.Name = "tBNachname";
+            this.tBNachname.Size = new System.Drawing.Size(100, 20);
+            this.tBNachname.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(292, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Nachname";
+            // 
+            // tbTelefonnummer
+            // 
+            this.tbTelefonnummer.Location = new System.Drawing.Point(355, 130);
+            this.tbTelefonnummer.Name = "tbTelefonnummer";
+            this.tbTelefonnummer.Size = new System.Drawing.Size(100, 20);
+            this.tbTelefonnummer.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(269, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Telefonnummer";
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 435);
+            this.Controls.Add(this.tbTelefonnummer);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tBNachname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.tbPresonen);
             this.Controls.Add(this.tbName);
             this.Controls.Add(this.label2);
@@ -115,5 +167,10 @@ namespace Projekt
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tBNachname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTelefonnummer;
+        private System.Windows.Forms.Label label5;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySqlConnector;
 
 namespace Projekt
 {
     public partial class Form4 : Form
     {
+
+
 
         private Form2 _form2;
         private Form3 _form3;
@@ -20,7 +23,7 @@ namespace Projekt
         public Form4(Form1 form1)
         {
             InitializeComponent();
-            _form1 = form1; // always assign Form1 here
+            _form1 = form1;
         }
 
         private void Form4_Load(object sender, EventArgs e)
@@ -32,19 +35,75 @@ namespace Projekt
         {
             _form2 = form2;
             _form3 = form3;
-            // optional: _form1 = form1ForNavigation; // we already have _form1 from constructor
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             string name = tbName.Text;
+            string nachname = tBNachname.Text;
+            string Telefonnummer = tbTelefonnummer.Text;
+            
             int personen = Convert.ToInt32(tbPresonen.Text);
 
-            // This will never be null now
+            SaveNameToDatabase(name, nachname, Telefonnummer);
+
             _form1.SetButtonColor(Color.Red);
 
+        }
 
+        private void SaveNameToDatabase(string name, string nachname, string Telefonnummer)
+        {
+            string connStr = "server=localhost;user=root;password=root;database=vesuv";
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "INSERT INTO gast(Vorname, Nachname, Telefonnummer) VALUES(@Vorname, @Nachname, @Telefonnummer)";
 
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    // Parameter zuweisen
+                    cmd.Parameters.AddWithValue("@Vorname", name);
+                    cmd.Parameters.AddWithValue("@Nachname", nachname);
+                    cmd.Parameters.AddWithValue("@Telefonnummer", Telefonnummer);
+
+                    // Ausführen
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            string connStr = "server=localhost;user=root;password=root;database=vesuv";
+            string query = "SELECT Vorname FROM gast";
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        
+                        object result = cmd.ExecuteScalar();
+
+                        if (result != null)
+                        {
+                            label3.Text = result.ToString(); 
+                        }
+                        else
+                        {
+                            label3.Text = "Keine Daten gefunden";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Fehler: " + ex.Message);
+                }
+            }
         }
     }
 }
