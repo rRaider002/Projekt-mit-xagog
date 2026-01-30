9d7ee8623348e82674bd3a9e32212fb5c801be9c
rRaider002 <Julian.Hillebrecht1012@gmail.com>
2026-01-30 09:56:04 +0100

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -55,3 +55,9 @@ CREATE TABLE Bestellposition (
   FOREIGN KEY (BestellungID) REFERENCES Bestellung(BestellungID),
   FOREIGN KEY (SpeiseID) REFERENCES Speise(SpeiseID)
 );
+
+CREATE TABLE Login (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL
+);
