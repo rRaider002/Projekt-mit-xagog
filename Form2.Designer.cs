dfc8376fafdf07e0d2b1e79ebf990d11b30d7426
rRaider002 <Julian.Hillebrecht1012@gmail.com>
2026-02-12 11:57:20 +0100

    Add files via upload

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -34,7 +34,21 @@ namespace Projekt
             this.btMenu1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tBName = new System.Windows.Forms.TextBox();
+            this.tBNachname = new System.Windows.Forms.TextBox();
+            this.tBTelefon = new System.Windows.Forms.TextBox();
+            this.tBPasswort = new System.Windows.Forms.TextBox();
+            this.tBjahr = new System.Windows.Forms.TextBox();
+            this.bTMitarbeiterErstellen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btLoeschen = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btMenu3
@@ -93,12 +107,134 @@ namespace Projekt
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
+            // tBName
+            // 
+            this.tBName.Location = new System.Drawing.Point(1522, 313);
+            this.tBName.Name = "tBName";
+            this.tBName.Size = new System.Drawing.Size(100, 20);
+            this.tBName.TabIndex = 8;
+            // 
+            // tBNachname
+            // 
+            this.tBNachname.Location = new System.Drawing.Point(1522, 352);
+            this.tBNachname.Name = "tBNachname";
+            this.tBNachname.Size = new System.Drawing.Size(100, 20);
+            this.tBNachname.TabIndex = 9;
+            // 
+            // tBTelefon
+            // 
+            this.tBTelefon.Location = new System.Drawing.Point(1522, 439);
+            this.tBTelefon.Name = "tBTelefon";
+            this.tBTelefon.Size = new System.Drawing.Size(100, 20);
+            this.tBTelefon.TabIndex = 10;
+            // 
+            // tBPasswort
+            // 
+            this.tBPasswort.Location = new System.Drawing.Point(1522, 484);
+            this.tBPasswort.Name = "tBPasswort";
+            this.tBPasswort.Size = new System.Drawing.Size(100, 20);
+            this.tBPasswort.TabIndex = 11;
+            // 
+            // tBjahr
+            // 
+            this.tBjahr.Location = new System.Drawing.Point(1522, 393);
+            this.tBjahr.Name = "tBjahr";
+            this.tBjahr.Size = new System.Drawing.Size(100, 20);
+            this.tBjahr.TabIndex = 12;
+            this.tBjahr.TextChanged += new System.EventHandler(this.tB_TextChanged);
+            // 
+            // bTMitarbeiterErstellen
+            // 
+            this.bTMitarbeiterErstellen.Location = new System.Drawing.Point(1536, 528);
+            this.bTMitarbeiterErstellen.Name = "bTMitarbeiterErstellen";
+            this.bTMitarbeiterErstellen.Size = new System.Drawing.Size(75, 23);
+            this.bTMitarbeiterErstellen.TabIndex = 13;
+            this.bTMitarbeiterErstellen.Text = "Erstellen";
+            this.bTMitarbeiterErstellen.UseVisualStyleBackColor = true;
+            this.bTMitarbeiterErstellen.Click += new System.EventHandler(this.bTMitarbeiterErstellen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1520, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Vorname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1519, 336);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Nachname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1519, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Geburtsjahr";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1519, 423);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Telefonnummer";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1519, 468);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Passwort";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(675, 408);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(641, 238);
+            this.dataGridView1.TabIndex = 19;
+            // 
+            // btLoeschen
+            // 
+            this.btLoeschen.Location = new System.Drawing.Point(1536, 557);
+            this.btLoeschen.Name = "btLoeschen";
+            this.btLoeschen.Size = new System.Drawing.Size(75, 23);
+            this.btLoeschen.TabIndex = 20;
+            this.btLoeschen.Text = "Löschen";
+            this.btLoeschen.UseVisualStyleBackColor = true;
+            this.btLoeschen.Click += new System.EventHandler(this.btLoeschen_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.btLoeschen);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bTMitarbeiterErstellen);
+            this.Controls.Add(this.tBjahr);
+            this.Controls.Add(this.tBPasswort);
+            this.Controls.Add(this.tBTelefon);
+            this.Controls.Add(this.tBNachname);
+            this.Controls.Add(this.tBName);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btMenu3);
             this.Controls.Add(this.btMenu2);
@@ -108,6 +244,7 @@ namespace Projekt
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,5 +256,18 @@ namespace Projekt
         private System.Windows.Forms.Button btMenu1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tBName;
+        private System.Windows.Forms.TextBox tBNachname;
+        private System.Windows.Forms.TextBox tBTelefon;
+        private System.Windows.Forms.TextBox tBPasswort;
+        private System.Windows.Forms.TextBox tBjahr;
+        private System.Windows.Forms.Button bTMitarbeiterErstellen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btLoeschen;
     }
 }
