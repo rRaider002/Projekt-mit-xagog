239417fb9e1644ad9e99db9d44f11a0a7b4c3b73
rRaider002 <Julian.Hillebrecht1012@gmail.com>
2026-02-12 11:55:14 +0100

    Update Form2.cs

--- Form2.cs
+++ Form2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySqlConnector;
 
 namespace Projekt
 {
@@ -42,7 +43,7 @@ namespace Projekt
 
         private void Form2_Load(object sender, EventArgs e)
         {
-
+            LoadData();
         }
 
         private void btMenu1_Click(object sender, EventArgs e)
@@ -64,6 +65,99 @@ namespace Projekt
             this.Hide();
             _form3.Show();
 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void tB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bTMitarbeiterErstellen_Click(object sender, EventArgs e)
+        {
+            string Vorname = tBName.Text;
+            string Nachname = tBNachname.Text;
+            string Geburtsjahr = tBjahr.Text;
+            string Telefonnummer = tBTelefon.Text;
+            string Passwort = tBPasswort.Text;
+            string Benutzername = Vorname + Nachname + Geburtsjahr;
+
+            SaveMitarbeiterToDatabase(Vorname, Nachname, Geburtsjahr, Telefonnummer, Passwort, Benutzername);
+
+            LoadData();
+        }
+
+        private void SaveMitarbeiterToDatabase(string Vorname, string Nachname, string Telefonnummer, string Passwort, string Geburtsjahr, string Benutzername)
+        {
+            string connStr = "server=localhost;user=root;password=root;database=vesuv";
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "INSERT INTO mitarbeiter(Vorname, Nachname, Telefon, passwort, geburtsjahr, benutzername) VALUES(@Vorname, @Nachname, @Telefon, @passwort, @geburtsjahr, @benutzername)";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+
+                    cmd.Parameters.AddWithValue("@Vorname", Vorname);
+                    cmd.Parameters.AddWithValue("@Nachname", Nachname);
+                    cmd.Parameters.AddWithValue("@Telefon", Telefonnummer);
+                    cmd.Parameters.AddWithValue("@passwort", Passwort);
+                    cmd.Parameters.AddWithValue("@geburtsjahr", int.Parse(Geburtsjahr));
+                    cmd.Parameters.AddWithValue("@benutzername", Benutzername);
+
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+
+        }
+
+        private void LoadData()
+        {
+            string connStr = "server=localhost;user=root;password=root;database=vesuv";
+            string query = "SELECT * FROM mitarbeiter";
+            { 
+
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    DataTable table = new DataTable();
+                    table.Load(reader);
+
+                    dataGridView1.AutoGenerateColumns = true;
+                    dataGridView1.DataSource = table;
+                }
+            }
+
+        }
+
+        private void btLoeschen_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int MitarbeiterID = Convert.ToInt32(dataGridView1.CurrentRow.Cells["MitarbeiterID"].Value);
+
+                string connStr = "server=localhost;user=root;password=root;database=vesuv";
+                string query = "DELETE FROM mitarbeiter WHERE MitarbeiterID = @MitarbeiterID";
+
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@MitarbeiterID", MitarbeiterID);
+                    cmd.ExecuteNonQuery();
+                }
+
+                LoadData();
+            }
         }
     }
 }
