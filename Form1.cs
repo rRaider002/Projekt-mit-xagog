671a2cbc7f35537a19aa0d9fa8ffcfddf189c302
rRaider002 <Julian.Hillebrecht1012@gmail.com>
2026-02-13 10:36:03 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,33 +12,79 @@ namespace Projekt
 {
     public partial class Form1 : Form
     {
-
         private Form2 _form2;
         private Form3 _form3;
         private Form4 _form4;
+        private Form5 _form5;
 
+        public string Buttonname { get; set; }
+        public string Nachname { get; set; }
         public Form1()
         {
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
+
+            button4.Click += AnyButton_Click;
+            button1.Click += AnyButton_Click;
+            button2.Click += AnyButton_Click;
+            button5.Click += AnyButton_Click;
+            button6.Click += AnyButton_Click;            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
-        public void SetOtherForms(Form2 form2, Form3 form3, Form4 form4)
+        public void SetOtherForms(Form2 form2, Form3 form3, Form4 form4,Form5 form5)
         {
             _form2 = form2;
             _form3 = form3;
             _form4 = form4;
+            _form5 = form5;
+        }
+
+
+        private Button _lastClickedButton;
+
+        public void AnyButton_Click(object sender, EventArgs e)
+        {
+            _form4.Hide();
+
+            _lastClickedButton = sender as Button;
+                          
+            _form4.Show();
+        }
+
+        public void SetLastClickedButtonText(string text)
+        {
+            Buttonname = text;
+
+            if (_lastClickedButton != null)
+            {
+                _lastClickedButton.Text = text;
+            }
         }
 
         public void SetButtonColor(Color color)
         {
-            button4.BackColor = color;
+            if (_lastClickedButton != null)
+            {
+                _lastClickedButton.BackColor = color;
+            }
+        }
+
+        public void ReceiveText(string text)
+        {
+            Nachname = text;
+            _lastClickedButton.Text = text;
+        }
+
+        public void ChangeButtonname(string text)
+        {
+            Buttonname = text;
+            _lastClickedButton.Text = text;
         }
 
         private void Menu1_Click(object sender, EventArgs e)
@@ -58,9 +104,35 @@ namespace Projekt
             _form3.Show();
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        public void button4_Click(object sender, EventArgs e)
         {
-            _form4.Show();
+
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+
+
+
+
+
     }
 }
