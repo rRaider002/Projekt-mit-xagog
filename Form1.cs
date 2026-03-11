97219c0f1be14b409cf59403c35932c853298b77
rRaider002 <Julian.Hillebrecht1012@gmail.com>
2026-03-11 16:13:06 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySqlConnector;
 
@@ -16,15 +10,15 @@ namespace Projekt
         private Form2 _form2;
         private Form3 _form3;
         private Form4 _form4;
-        private Form5 _form5;
+        private Form6 _form6;
         public Button _lastClickedButton;
 
-        public string Nachname { get; set; }
         public Form1()
         {
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
+            this.Load += Form1_Load;
 
             Tisch1.Click += AnyButton_Click;
             Tisch2.Click += AnyButton_Click;
@@ -68,49 +62,169 @@ namespace Projekt
             Tisch40.Click += AnyButton_Click;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
-        }
-
-        public void SetOtherForms(Form2 form2, Form3 form3, Form4 form4,Form5 form5)
+        public void SetOtherForms(Form2 form2, Form3 form3, Form4 form4, Form6 form6)
         {
             _form2 = form2;
             _form3 = form3;
             _form4 = form4;
-            _form5 = form5;
+            _form6 = form6;
         }
 
-        public void AnyButton_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            _form4.Hide();
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add(1);
+            comboBox1.Items.Add(2);
+            comboBox1.Items.Add(3);
+            comboBox1.Items.Add(4);
+            comboBox1.SelectedIndex = 0;
 
-            _lastClickedButton = sender as Button;
-
-            _form4.SetTischName(_lastClickedButton.Name);
-
-            _form4.Show();
-
-
+            LadeTischStatus();
         }
 
-        public void SaveTischNameToDatabase(string TischName)
-        {   
-            string connStr = "server=localhost;user=root;password=root;database=vesuv";
-            using (MySqlConnection conn = new MySqlConnection(connStr))
-            {
-                conn.Open();
-                string query = "INSERT INTO tischName(TischName) values(@TischName)";
+        public int GetSelectedSlot()
+        {
+            if (comboBox1.SelectedItem == null)
+                return -1;
 
-                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+            return Convert.ToInt32(comboBox1.SelectedItem);
+        }
+
+        public void LadeTischStatus()
+        {
+            if (comboBox1.SelectedItem == null)
+                return;
+
+            int slot = GetSelectedSlot();
+
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button button && button.Name.StartsWith("Tisch"))
                 {
-                    cmd.Parameters.AddWithValue("@TischName", TischName);
-                    cmd.ExecuteNonQuery();
+                    int tischID = GetTischIDFromName(button.Name);
+
+                    if (tischID == -1)
+                        continue;
+
+                    if (IstTischReserviert(tischID, slot))
+                    {
+                        button.BackColor = Color.Red;
+                        button.Enabled = true;
+
+                        string nachname = GetNachnameVonReservierung(tischID, slot);
+
+                        if (!string.IsNullOrWhiteSpace(nachname))
+                            button.Text = nachname;
+                        else
+                            button.Text = "Reserviert";
+                    }
+                    else
+                    {
+                        button.BackColor = Color.Lime;
+                        button.Text = button.Name;
+                        button.Enabled = true;
+                    }
                 }
             }
         }
 
 
+        private string GetNachnameVonReservierung(int tischID, int slot)
+        {
+            string connStr = "server=localhost;user=root;password=root;database=vesuv";
+
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+
+                string query = @"
+        SELECT g.Nachname
+        FROM Reservierung r
+        INNER JOIN Gast g ON r.GastID = g.GastID
+        WHERE r.TischID = @tischID
+          AND r.Slot = @slot
+          AND r.IsDeleted = 0
+        ORDER BY r.ReservierungID DESC
+        LIMIT 1";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@tischID", tischID);
+                    cmd.Parameters.AddWithValue("@slot", slot);
+
+                    object result = cmd.ExecuteScalar();
+
+                    if (result != null)
+                        return result.ToString();
+                }
+            }
+
+            return "";
+        }
+
+        private bool IstTischReserviert(int tischID, int slot)
+        {
+            string connStr = "server=localhost;user=root;password=root;database=vesuv";
+
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+
+                string query = @"
+                SELECT COUNT(*)
+                FROM Reservierung
+                WHERE TischID = @tischID
+                    AND Slot = @slot
+                    AND IsDeleted = 0";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@tischID", tischID);
+                    cmd.Parameters.AddWithValue("@slot", slot);
+
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    return count > 0;
+                }
+            }
+        }
+
+        private int GetTischIDFromName(string tischName)
+        {
+            string connStr = "server=localhost;user=root;password=root;database=vesuv";
+
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+
+                string query = "SELECT TischID FROM Tisch WHERE TischName = @name";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@name", tischName);
+
+                    object result = cmd.ExecuteScalar();
+                    if (result != null)
+                        return Convert.ToInt32(result);
+                }
+            }
+
+            return -1;
+        }
+
+        public void AnyButton_Click(object sender, EventArgs e)
+        {
+            _form4.Hide();
+            _lastClickedButton = sender as Button;
+
+            if (_lastClickedButton == null)
+                return;
+
+            int tischID = GetTischIDFromName(_lastClickedButton.Name);
+
+
+            _form4.SetTischName(_lastClickedButton.Name);
+            _form4.Show();
+        }
 
         public void SetButtonColor(Color color)
         {
@@ -118,17 +232,6 @@ namespace Projekt
             {
                 _lastClickedButton.BackColor = color;
             }
-        }
-
-        public void ReceiveText(string text)
-        {
-            Nachname = text;
-            _lastClickedButton.Text = text;
-        }
-
-        private void Menu1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void Menu2_Click_1(object sender, EventArgs e)
@@ -139,37 +242,51 @@ namespace Projekt
 
         private void Menu3_Click_1(object sender, EventArgs e)
         {
-            this.Hide();
-            _form3.Show();
+
         }
 
-        public void button4_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
-            
         }
 
         private void Tisch2_Click(object sender, EventArgs e)
         {
-
         }
 
         private void Tisch3_Click(object sender, EventArgs e)
         {
-
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-
         }
 
         private void pictureBox5_Click(object sender, EventArgs e)
         {
-
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
+        }
+
+        private void Tisch9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tisch13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tisch17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LadeTischStatus();
 
         }
     }
