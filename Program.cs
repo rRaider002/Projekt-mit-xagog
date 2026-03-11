aa4699a8bfc7b6d268c0b12f1a323cf380ee300c
rRaider002 <Julian.Hillebrecht1012@gmail.com>
2026-03-11 16:12:37 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,9 +8,7 @@ namespace Projekt
 {
     static class Program
     {
-        /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
@@ -19,14 +17,18 @@ namespace Projekt
 
             Form1 form1 = new Form1();
             Form2 form2 = new Form2();
-            Form3 form3 = new Form3();
-            Form4 form4 = new Form4(form1); 
+            Form3 form3 = new Form3(form1);
+            Form4 form4 = new Form4(form1);
+            Login form5 = new Login();
+            Form6 form6 = new Form6();
 
             
-            form1.SetOtherForms(form2, form3, form4);
+            form1.SetOtherForms(form2, form3, form4, form6);
             form2.SetOtherForms(form1, form3, form4);
             form3.SetOtherForms(form2, form1, form4);
             form4.SetOtherForms(form2, form3, form1);
+            form5.SetOtherForms(form1);
+            form6.SetOtherForms(form1, form2);
 
             Application.Run(form1);
 
