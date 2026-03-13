8a7451b3a3428d9bbeb6125e22b9a34062083b28
rRaider002 <Julian.Hillebrecht1012@gmail.com>
2026-03-13 10:43:10 +0100

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -72,6 +72,7 @@ CREATE TABLE Reservierung (
   FOREIGN KEY (TischID) REFERENCES Tisch(TischID)
 );	
 
+SELECT TischID, TischName FROM Tisch;
 
 
 -- =========================
@@ -89,6 +90,13 @@ CREATE TABLE Bestellung (
   FOREIGN KEY (GastID) REFERENCES Gast(GastID)
 );
 
+Select * from Bestellung;
+
+INSERT INTO Gast (Vorname, Nachname, Telefon) 
+VALUES ('Laufkundschaft', 'Allgemein', '0000');
+
+SELECT * FROM Gast;
+
 -- =========================
 -- BESTELLPOSITION (Positionen)
 -- =========================
@@ -103,7 +111,17 @@ CREATE TABLE Bestellposition (
   FOREIGN KEY (SpeiseID) REFERENCES Speise(SpeiseID)
 );
 
-
+SELECT 
+    B.BestellungID AS 'Nr', 
+    DATE_FORMAT(B.Zeitpunkt, '%H:%i') AS 'Uhrzeit', 
+    T.TischName AS 'Tisch', 
+    SUM(BP.Menge * BP.Einzelpreis) AS 'Summe'
+FROM Bestellung B
+JOIN Tisch T ON B.TischID = T.TischID
+JOIN Bestellposition BP ON B.BestellungID = BP.BestellungID
+WHERE DATE(B.Zeitpunkt) = CURDATE()
+GROUP BY B.BestellungID, B.Zeitpunkt, T.TischName 
+ORDER BY B.Zeitpunkt DESC;
 
 INSERT INTO Mitarbeiter (Vorname ,Nachname,Telefon,passwort_hash,geburtsjahr, bereich)
 VALUES
@@ -123,7 +141,7 @@ VALUES
 ('Nina', 'Richter', '01666999000', 'Nina654', 1992,'Kellner');
 
 	
-
+Select * from Speise;
 
 INSERT INTO Tisch (TischName, Plaetze, Lage, KellnerID)
 VALUES
@@ -169,78 +187,78 @@ VALUES
 ('Tisch40',8,'Fenster',11);
 
 
-INSERT INTO Speise (Bezeichnung, Beschreibung, Preis, SpeiseType,Kategorie, Zutaten)
+INSERT INTO Speise (Bezeichnung, Beschreibung, Preis, SpeiseType, Kategorie, Zutaten)
 VALUES 
 -- Speisen
 
 -- PIZZA
-('Margherita', 'Pizza mit Tomaten und Mozzarella', 8.90, 'Hauptgericht', 'Tomaten, Mozzarella, Basilikum'),
-('Salami', 'Pizza mit Tomatensauce, Mozzarella und Salami', 9.50, 'Hauptgericht', 'Tomaten, Mozzarella, Salami'),
-('Prosciutto', 'Pizza mit Tomatensauce, Mozzarella und Kochschinken', 9.80, 'Hauptgericht', 'Tomaten, Mozzarella, Kochschinken'),
-('Funghi', 'Pizza mit Tomatensauce, Mozzarella und Champignons', 9.20, 'Hauptgericht', 'Tomaten, Mozzarella, Champignons'),
-('Hawaii', 'Pizza mit Schinken und Ananas', 10.00, 'Hauptgericht', 'Tomaten, Mozzarella, Schinken, Ananas'),
-('Quattro Stagioni', 'Pizza mit vier verschiedenen Belägen', 11.50, 'Hauptgericht', 'Tomaten, Mozzarella, Schinken, Artischocken, Oliven, Champignons'),
-('Quattro Formaggi', 'Pizza mit vier Käsesorten', 10.90, 'Hauptgericht', 'Mozzarella, Gorgonzola, Parmesan, Emmentaler'),
-('Diavola', 'Scharfe Pizza mit Salami und Chili', 10.50, 'Hauptgericht', 'Tomaten, Mozzarella, scharfe Salami, Chili'),
+('Margherita', 'Pizza mit Tomaten und Mozzarella', 8.90, 'Hauptgericht','Pizza', 'Tomaten, Mozzarella, Basilikum'),
+('Salami', 'Pizza mit Tomatensauce, Mozzarella und Salami', 9.50, 'Hauptgericht','Pizza', 'Tomaten, Mozzarella, Salami'),
+('Prosciutto', 'Pizza mit Tomatensauce, Mozzarella und Kochschinken', 9.80, 'Hauptgericht','Pizza', 'Tomaten, Mozzarella, Kochschinken'),
+('Funghi', 'Pizza mit Tomatensauce, Mozzarella und Champignons', 9.20, 'Hauptgericht','Pizza', 'Tomaten, Mozzarella, Champignons'),
+('Hawaii', 'Pizza mit Schinken und Ananas', 10.00, 'Hauptgericht','Pizza', 'Tomaten, Mozzarella, Schinken, Ananas'),
+('Quattro Stagioni', 'Pizza mit vier verschiedenen Belägen', 11.50, 'Hauptgericht','Pizza', 'Tomaten, Mozzarella, Schinken, Artischocken, Oliven, Champignons'),
+('Quattro Formaggi', 'Pizza mit vier Käsesorten', 10.90, 'Hauptgericht','Pizza', 'Mozzarella, Gorgonzola, Parmesan, Emmentaler'),
+('Diavola', 'Scharfe Pizza mit Salami und Chili', 10.50, 'Hauptgericht','Pizza', 'Tomaten, Mozzarella, scharfe Salami, Chili'),
 -- Pasta
-('Spaghetti Bolognese', 'Pasta mit Rinderhackfleisch-Tomatensauce', 11.50, 'Hauptgericht', 'Spaghetti, Rinderhackfleisch, Tomaten, Zwiebeln'),
-('Spaghetti Carbonara', 'Pasta mit Ei, Käse und Speck', 12.00, 'Hauptgericht', 'Spaghetti, Ei, Pecorino, Speck'),
-('Penne Arrabbiata', 'Pasta mit scharfer Tomatensauce', 10.50, 'Hauptgericht', 'Penne, Tomaten, Knoblauch, Chili'),
-('Lasagne al Forno', 'Überbackene Lasagne mit Fleischragù', 13.50, 'Hauptgericht', 'Lasagneplatten, Rinderhackfleisch, Tomaten, Béchamelsauce'),
-('Tagliatelle al Pesto', 'Bandnudeln mit Basilikum-Pesto', 11.00, 'Hauptgericht', 'Tagliatelle, Basilikum, Pinienkerne, Parmesan, Olivenöl'),
+('Spaghetti Bolognese', 'Pasta mit Rinderhackfleisch-Tomatensauce', 11.50, 'Hauptgericht','Pasta', 'Spaghetti, Rinderhackfleisch, Tomaten, Zwiebeln'),
+('Spaghetti Carbonara', 'Pasta mit Ei, Käse und Speck', 12.00, 'Hauptgericht','Pasta', 'Spaghetti, Ei, Pecorino, Speck'),
+('Penne Arrabbiata', 'Pasta mit scharfer Tomatensauce', 10.50, 'Hauptgericht','Pasta', 'Penne, Tomaten, Knoblauch, Chili'),
+('Lasagne al Forno', 'Überbackene Lasagne mit Fleischragù', 13.50, 'Hauptgericht','Pasta', 'Lasagneplatten, Rinderhackfleisch, Tomaten, Béchamelsauce'),
+('Tagliatelle al Pesto', 'Bandnudeln mit Basilikum-Pesto', 11.00, 'Hauptgericht','Pasta', 'Tagliatelle, Basilikum, Pinienkerne, Parmesan, Olivenöl'),
 -- Salat
-('Caesar Salat', 'Römersalat mit Caesar-Dressing und Croutons', 9.50, 'Vorspeise', 'Römersalat, Croutons, Parmesan, Caesar-Dressing'),
-('Griechischer Salat', 'Salat mit Feta und Oliven', 8.90, 'Vorspeise', 'Tomaten, Gurken, Feta, Oliven, Zwiebeln'),
-('Gemischter Salat', 'Bunter Salat mit verschiedenen Blattsalaten', 7.50, 'Vorspeise', 'Blattsalat, Tomaten, Gurken, Karotten'),
-('Thunfischsalat', 'Salat mit Thunfisch und Zwiebeln', 9.80, 'Vorspeise', 'Blattsalat, Thunfisch, Zwiebeln, Oliven'),
-('Caprese', 'Tomaten mit Mozzarella und Basilikum', 8.50, 'Vorspeise', 'Tomaten, Mozzarella, Basilikum, Olivenöl'),
-('Hähnchensalat', 'Salat mit gegrilltem Hähnchenbrustfilet', 10.90, 'Hauptgericht', 'Blattsalat, Hähnchenbrust, Tomaten, Gurken'),
-('Rucolasalat', 'Rucola mit Parmesan und Kirschtomaten', 8.20, 'Vorspeise', 'Rucola, Parmesan, Kirschtomaten, Balsamico'),
+('Caesar Salat', 'Römersalat mit Caesar-Dressing und Croutons', 9.50, 'Vorspeise','Salat', 'Römersalat, Croutons, Parmesan, Caesar-Dressing'),
+('Griechischer Salat', 'Salat mit Feta und Oliven', 8.90, 'Vorspeise','Salat', 'Tomaten, Gurken, Feta, Oliven, Zwiebeln'),
+('Gemischter Salat', 'Bunter Salat mit verschiedenen Blattsalaten', 7.50, 'Vorspeise','Salat', 'Blattsalat, Tomaten, Gurken, Karotten'),
+('Thunfischsalat', 'Salat mit Thunfisch und Zwiebeln', 9.80, 'Vorspeise','Salat', 'Blattsalat, Thunfisch, Zwiebeln, Oliven'),
+('Caprese', 'Tomaten mit Mozzarella und Basilikum', 8.50, 'Vorspeise','Salat', 'Tomaten, Mozzarella, Basilikum, Olivenöl'),
+('Hähnchensalat', 'Salat mit gegrilltem Hähnchenbrustfilet', 10.90, 'Hauptgericht','Salat', 'Blattsalat, Hähnchenbrust, Tomaten, Gurken'),
+('Rucolasalat', 'Rucola mit Parmesan und Kirschtomaten', 8.20, 'Vorspeise','Salat', 'Rucola, Parmesan, Kirschtomaten, Balsamico'),
 -- Antipasti
-('Bruschetta', 'Geröstetes Brot mit Tomaten und Basilikum', 6.50, 'Vorspeise', 'Baguette, Tomaten, Basilikum, Olivenöl, Knoblauch'),
-('Vitello Tonnato', 'Kalbfleisch mit Thunfischsauce', 11.50, 'Vorspeise', 'Kalbfleisch, Thunfisch, Kapern, Mayonnaise'),
-('Carpaccio', 'Hauchdünnes Rindfleisch mit Parmesan', 12.00, 'Vorspeise', 'Rinderfilet, Parmesan, Rucola, Olivenöl'),
-('Antipasti Misti', 'Gemischte italienische Vorspeisenplatte', 13.50, 'Vorspeise', 'Gegrilltes Gemüse, Oliven, Salami, Käse'),
-('Gegrillte Zucchini', 'Marinierte und gegrillte Zucchinischeiben', 7.20, 'Vorspeise', 'Zucchini, Olivenöl, Knoblauch, Kräuter'),
-('Gegrillte Paprika', 'Marinierte Paprika mit Olivenöl', 7.20, 'Vorspeise', 'Paprika, Olivenöl, Knoblauch, Kräuter'),
+('Bruschetta', 'Geröstetes Brot mit Tomaten und Basilikum', 6.50, 'Vorspeise','Antipasti', 'Baguette, Tomaten, Basilikum, Olivenöl, Knoblauch'),
+('Vitello Tonnato', 'Kalbfleisch mit Thunfischsauce', 11.50, 'Vorspeise','Antipasti', 'Kalbfleisch, Thunfisch, Kapern, Mayonnaise'),
+('Carpaccio', 'Hauchdünnes Rindfleisch mit Parmesan', 12.00, 'Vorspeise','Antipasti', 'Rinderfilet, Parmesan, Rucola, Olivenöl'),
+('Antipasti Misti', 'Gemischte italienische Vorspeisenplatte', 13.50, 'Vorspeise','Antipasti', 'Gegrilltes Gemüse, Oliven, Salami, Käse'),
+('Gegrillte Zucchini', 'Marinierte und gegrillte Zucchinischeiben', 7.20, 'Vorspeise','Antipasti', 'Zucchini, Olivenöl, Knoblauch, Kräuter'),
+('Gegrillte Paprika', 'Marinierte Paprika mit Olivenöl', 7.20, 'Vorspeise','Antipasti', 'Paprika, Olivenöl, Knoblauch, Kräuter'),
 -- Suppe
-('Minestrone', 'Italienische Gemüsesuppe mit Pasta', 6.90, 'Vorspeise', 'Karotten, Zucchini, Sellerie, Tomaten, Pasta, Bohnen'),
-('Tomatensuppe', 'Cremige Tomatensuppe mit Basilikum', 5.90, 'Vorspeise', 'Tomaten, Zwiebeln, Knoblauch, Sahne, Basilikum'),
-('Zucchinisuppe', 'Feine Suppe aus frischer Zucchini', 6.50, 'Vorspeise', 'Zucchini, Zwiebeln, Kartoffeln, Gemüsebrühe'),
-('Kartoffelsuppe', 'Herzhafte Suppe mit Kartoffeln und Gemüse', 6.50, 'Vorspeise', 'Kartoffeln, Karotten, Lauch, Sellerie, Sahne'),
-('Hühnersuppe', 'Klassische Suppe mit Huhn und Gemüse', 7.50, 'Vorspeise', 'Hühnerfleisch, Karotten, Sellerie, Nudeln, Petersilie'),
-('Gulaschsuppe', 'Würzige Rindfleischsuppe mit Paprika', 8.50, 'Vorspeise', 'Rindfleisch, Paprika, Zwiebeln, Kartoffeln, Paprikapulver'),
+('Minestrone', 'Italienische Gemüsesuppe mit Pasta', 6.90, 'Vorspeise','Suppe', 'Karotten, Zucchini, Sellerie, Tomaten, Pasta, Bohnen'),
+('Tomatensuppe', 'Cremige Tomatensuppe mit Basilikum', 5.90, 'Vorspeise','Suppe', 'Tomaten, Zwiebeln, Knoblauch, Sahne, Basilikum'),
+('Zucchinisuppe', 'Feine Suppe aus frischer Zucchini', 6.50, 'Vorspeise','Suppe', 'Zucchini, Zwiebeln, Kartoffeln, Gemüsebrühe'),
+('Kartoffelsuppe', 'Herzhafte Suppe mit Kartoffeln und Gemüse', 6.50, 'Vorspeise','Suppe', 'Kartoffeln, Karotten, Lauch, Sellerie, Sahne'),
+('Hühnersuppe', 'Klassische Suppe mit Huhn und Gemüse', 7.50, 'Vorspeise','Suppe', 'Hühnerfleisch, Karotten, Sellerie, Nudeln, Petersilie'),
+('Gulaschsuppe', 'Würzige Rindfleischsuppe mit Paprika', 8.50, 'Vorspeise','Suppe', 'Rindfleisch, Paprika, Zwiebeln, Kartoffeln, Paprikapulver'),
 
 -- Getränke
 
 -- Alkoholfrei
-('Orangensaft', 'Frisch gepresster Orangensaft', 3.80, 'Getränk', 'Orangensaft'),
-('Cola', 'Klassische Cola', 2.90, 'Getränk', 'Wasser, Zucker, Kohlensäure, Karamell, Aromen'),
-('Fanta', 'Erfrischende Orangenlimonade', 2.90, 'Getränk', 'Wasser, Zucker, Kohlensäure, Orangenaroma'),
-('Sprite', 'Zitronen-Limetten-Limonade', 2.90, 'Getränk', 'Wasser, Zucker, Kohlensäure, Zitronen- und Limettenaroma'),
-('Mineralwasser', 'Sprudelndes Mineralwasser', 2.50, 'Getränk', 'Mineralwasser'),
-('Stillwasser', 'Ohne Kohlensäure', 2.50, 'Getränk', 'Mineralwasser'),
-('Eistee Pfirsich', 'Pfirsich-Eistee kalt serviert', 3.20, 'Getränk', 'Wasser, Tee, Zucker, Pfirsicharoma'),
-('Eistee Zitrone', 'Zitronen-Eistee kalt serviert', 3.20, 'Getränk', 'Wasser, Tee, Zucker, Zitronenaroma'),
-('Limonade Zitrone', 'Hausgemachte Zitronenlimonade', 3.00, 'Getränk', 'Wasser, Zitronensaft, Zucker'),
-('Limonade Himbeere', 'Hausgemachte Himbeerlimonade', 3.20, 'Getränk', 'Wasser, Himbeersaft, Zucker'),
+('Orangensaft', 'Frisch gepresster Orangensaft', 3.80, 'Getränk','Alkoholfrei', 'Orangensaft'),
+('Cola', 'Klassische Cola', 2.90, 'Getränk','Alkoholfrei', 'Wasser, Zucker, Kohlensäure, Karamell, Aromen'),
+('Fanta', 'Erfrischende Orangenlimonade', 2.90, 'Getränk','Alkoholfrei', 'Wasser, Zucker, Kohlensäure, Orangenaroma'),
+('Sprite', 'Zitronen-Limetten-Limonade', 2.90, 'Getränk','Alkoholfrei', 'Wasser, Zucker, Kohlensäure, Zitronen- und Limettenaroma'),
+('Mineralwasser', 'Sprudelndes Mineralwasser', 2.50, 'Getränk','Alkoholfrei', 'Mineralwasser'),
+('Stillwasser', 'Ohne Kohlensäure', 2.50, 'Getränk','Alkoholfrei', 'Mineralwasser'),
+('Eistee Pfirsich', 'Pfirsich-Eistee kalt serviert', 3.20, 'Getränk','Alkoholfrei', 'Wasser, Tee, Zucker, Pfirsicharoma'),
+('Eistee Zitrone', 'Zitronen-Eistee kalt serviert', 3.20, 'Getränk','Alkoholfrei', 'Wasser, Tee, Zucker, Zitronenaroma'),
+('Limonade Zitrone', 'Hausgemachte Zitronenlimonade', 3.00, 'Getränk','Alkoholfrei', 'Wasser, Zitronensaft, Zucker'),
+('Limonade Himbeere', 'Hausgemachte Himbeerlimonade', 3.20, 'Getränk','Alkoholfrei', 'Wasser, Himbeersaft, Zucker'),
 -- Alkoholische
-('Bier', 'Klassisches Pils vom Fass', 4.50, 'Getränk', 'Wasser, Malz, Hopfen, Hefe'),
-('Weißwein', 'Trockener Weißwein aus Italien', 5.50, 'Getränk', 'Weintrauben'),
-('Rotwein', 'Trockener Rotwein aus Italien', 5.50, 'Getränk', 'Weintrauben'),
-('Prosecco', 'Italienischer Schaumwein', 6.00, 'Getränk', 'Weintrauben, Kohlensäure'),
-('Aperol Spritz', 'Aperol, Prosecco und Soda', 7.50, 'Getränk', 'Aperol, Prosecco, Soda, Orangenscheibe'),
-('Campari Soda', 'Bitterer Campari mit Soda', 6.50, 'Getränk', 'Campari, Soda, Eis'),
-('Hugo', 'Prosecco, Holunderblütensirup und Minze', 7.00, 'Getränk', 'Prosecco, Holunderblütensirup, Minze, Soda'),
-('Gin Tonic', 'Gin mit Tonic Water', 8.00, 'Getränk', 'Gin, Tonic Water, Limette, Eiswürfel'),
+('Bier', 'Klassisches Pils vom Fass', 4.50, 'Getränk','Alkoholische', 'Wasser, Malz, Hopfen, Hefe'),
+('Weißwein', 'Trockener Weißwein aus Italien', 5.50, 'Getränk','Alkoholische', 'Weintrauben'),
+('Rotwein', 'Trockener Rotwein aus Italien', 5.50, 'Getränk','Alkoholische', 'Weintrauben'),
+('Prosecco', 'Italienischer Schaumwein', 6.00, 'Getränk','Alkoholische', 'Weintrauben, Kohlensäure'),
+('Aperol Spritz', 'Aperol, Prosecco und Soda', 7.50, 'Getränk','Alkoholische', 'Aperol, Prosecco, Soda, Orangenscheibe'),
+('Campari Soda', 'Bitterer Campari mit Soda', 6.50, 'Getränk','Alkoholische', 'Campari, Soda, Eis'),
+('Hugo', 'Prosecco, Holunderblütensirup und Minze', 7.00, 'Getränk','Alkoholische', 'Prosecco, Holunderblütensirup, Minze, Soda'),
+('Gin Tonic', 'Gin mit Tonic Water', 8.00, 'Getränk','Alkoholische', 'Gin, Tonic Water, Limette, Eiswürfel'),
 
 -- Dessert
-('Tiramisu', 'Italienisches Dessert mit Mascarpone und Kaffee', 6.50, 'Dessert', 'Mascarpone, Kaffee, Löffelbiskuit, Zucker, Eier, Kakao'),
-('Panna Cotta', 'Cremige Vanille-Pudding-Spezialität', 5.90, 'Dessert', 'Sahne, Zucker, Vanille, Gelatine, Beeren'),
-('Gelato', 'Italienisches Speiseeis', 4.50, 'Dessert', 'Milch, Zucker, Eigelb, Vanille, Fruchtaromen'),
-('Profiteroles', 'Brandteigkugeln gefüllt mit Sahne und Schokolade', 6.80, 'Dessert', 'Brandteig, Sahne, Schokolade'),
-('Creme Brûlée', 'Karamellisierte Vanillecreme', 6.90, 'Dessert', 'Sahne, Zucker, Eigelb, Vanille'),
-('Obstsalat', 'Frischer Obstsalat der Saison', 5.50, 'Dessert', 'Äpfel, Birnen, Trauben, Kiwi, Orange, Honig');
+('Tiramisu', 'Italienisches Dessert mit Mascarpone und Kaffee', 6.50, 'Dessert','Dessert', 'Mascarpone, Kaffee, Löffelbiskuit, Zucker, Eier, Kakao'),
+('Panna Cotta', 'Cremige Vanille-Pudding-Spezialität', 5.90, 'Dessert','Dessert', 'Sahne, Zucker, Vanille, Gelatine, Beeren'),
+('Gelato', 'Italienisches Speiseeis', 4.50, 'Dessert','Dessert', 'Milch, Zucker, Eigelb, Vanille, Fruchtaromen'),
+('Profiteroles', 'Brandteigkugeln gefüllt mit Sahne und Schokolade', 6.80,'Dessert', 'Dessert', 'Brandteig, Sahne, Schokolade'),
+('Creme Brûlée', 'Karamellisierte Vanillecreme', 6.90, 'Dessert','Dessert', 'Sahne, Zucker, Eigelb, Vanille'),
+('Obstsalat', 'Frischer Obstsalat der Saison', 5.50, 'Dessert','Dessert', 'Äpfel, Birnen, Trauben, Kiwi, Orange, Honig');
 
 
 -- Umsatz Tag
@@ -276,3 +294,18 @@ WHERE YEAR(Bestellung.Zeitpunkt) = YEAR(CURDATE());
 SELECT SUM(Bestellposition.Menge * Bestellposition.Einzelpreis) AS GesamtUmsatz
 FROM Bestellung
 JOIN Bestellposition ON Bestellung.BestellungID = Bestellposition.BestellungID;
+
+
+-- Umsatz Kellner
+SELECT 
+    M.Vorname, 
+    M.Nachname, 
+    SUM(BP.Menge * BP.Einzelpreis) AS 'Umsatz'
+FROM Mitarbeiter M
+-- Wir verknüpfen den Mitarbeiter über die KellnerID des Tisches
+JOIN Tisch T ON M.MitarbeiterID = T.KellnerID
+-- Dann verknüpfen wir die Bestellungen, die an diesem Tisch gemacht wurden
+JOIN Bestellung B ON T.TischID = B.TischID
+JOIN Bestellposition BP ON B.BestellungID = BP.BestellungID
+GROUP BY M.MitarbeiterID, M.Vorname, M.Nachname
+ORDER BY Umsatz DESC;
